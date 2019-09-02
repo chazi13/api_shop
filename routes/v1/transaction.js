dcec834abd563422ad38f3ab0c3677fbe631bf9e
Muhamad Chairul Aziz <m.chairul669@gmail.com>
2019-09-02 10:03:30 +0700

    Transaction module

--- routes/v1/transaction.js
+++ routes/v1/transaction.js
@@ -4,7 +4,7 @@ const auth = require('../../middlewares/auth');
 const TransactionController = require('../../controller/transaction');
 
 router.post('/register_table', TransactionController.registerTable);
-router.get('/view_bill', auth.authorize, auth.errAuth, OrderController.viewBill);
+router.get('/view_bill', auth.authorize, auth.errAuth, TransactionController.viewBill);
 router.patch('/confirm', auth.authorize, auth.errAuth, TransactionController.confirm);
 
 module.exports = router
